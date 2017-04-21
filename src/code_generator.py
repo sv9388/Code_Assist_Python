a86edb2d29fe4e6809819fa334e844ccb2d74b4d
Ubuntu <ubuntu@ip-172-31-3-65.us-east-2.compute.internal>
2017-04-21 11:54:57 +0000

    SO changes

--- src/code_generator.py
+++ src/code_generator.py
@@ -6,11 +6,11 @@ from bs4 import BeautifulSoup as bs
 server_url_fs = "https://api.stackexchange.com/2.2/search/advanced?%s"
 
 #TODO: Fill Client key after oayth impl
-params = { "page" : 1 , "pagesize" : 100, "order" : "desc" , "sort" : "relevance" , "q" : "" , "tagged" : "python;python-2.7" , "site" : "stackoverflow", "accepted" : "True" }
+params = { "page" : 1 , "pagesize" : 100, "order" : "desc" , "sort" : "relevance" , "q" : "" , "tagged" : "python" , "site" : "stackoverflow", "accepted" : "True" }
 
 answer_url_fs = "https://stackoverflow.com/a/%d"
 
-def get_all_answers(keywords, page = 1, access_token = None, app_key = None):
+def get_all_answers(ip_str, keywords, page = 1, access_token = None, app_key = None):
 	items = []
 	errors = ''
 
@@ -18,7 +18,7 @@ def get_all_answers(keywords, page = 1, access_token = None, app_key = None):
 		errors = 'No valid keywords detected. Please post a specific question'
 		return [], errors 
 
-	params["q"] = ' '.join(keywords)
+	params["q"] = ip_str #' '.join(keywords)
 	params["page"] = page
 	if access_token and app_key:
 		#print access_token, app_key
@@ -38,7 +38,7 @@ def get_all_answers(keywords, page = 1, access_token = None, app_key = None):
 
 	items = js_data['items']
 	if js_data['has_more']:
-		next_items, new_errors = get_all_answers(keywords, page + 1, access_token, app_key)
+		next_items, new_errors = get_all_answers(ip_str, keywords, page + 1, access_token, app_key)
 		items += next_items
 		errors += ' ' + new_errors
 	else:
@@ -48,54 +48,55 @@ def get_all_answers(keywords, page = 1, access_token = None, app_key = None):
 #Code is compatible with https://github.com/google/code-prettify html
 def get_code_in_answer_id(answer_id):
 	start_line = u"# CODE ASSIST: TODO: Rename variables in generated code\n"
-	end_line = u"# CODE ASSIST: End of generated code"
+	end_line = u"\n# CODE ASSIST: End of generated code"
 
 	url = answer_url_fs % answer_id
 	r = requests.get(url)
 	if r.status_code != 200:
 		errors = "Invalid URL at %s" % url 
-		return "", errors
+		return "", url,  errors
  
 	soup = bs(r.text, "html.parser")
 	answer_div = soup.find("div", attrs = {'data-answerid' : str(answer_id)})
-	#TODO: Multiple code snippets in accepted answer. So rewrite this aprt. 
-	code_div = answer_div.find("code")
+	print type(answer_div), url
+	pre_divs = answer_div.find_all("pre")
 
-	code = ''  
+	code_snippets = []
 	errors = ''  
-	if not code_div:
-		errors = u"#No valid code snippets found. Break the query into subproblems or refer to the details in %s" %url
+	if  len(pre_divs) == 0:
+		errors = u"#No valid code snippets found. Break the query into subproblems. The closest match to your query is at %s" %url
 	else:
-		code = code_div.text
+		for div in pre_divs:
+			code_snippets.append(div.find('code').text.strip())
 
-	return start_line + code + end_line, errors
+	return start_line + '\n## or \n'.join(code_snippets) + end_line, url, errors
 	
 
-def get_code(keywords, access_token = None, app_key = None):
+def get_code(ip_str, keywords, access_token = None, app_key = None):
 	code = ''
 	errors = ''
-	answers, errors = get_all_answers(keywords, 1, access_token, app_key)
+	answers, errors = get_all_answers(ip_str, keywords, 1, access_token, app_key)
 	if len(answers) == 0:
 		if len(errors) == 0:
 			errors = "No answers retrieved. Break it into subproblems and retry  per problem or rephrase the existing question"
-		return code, errors
+		return code, '', errors
 
 	#TODO: Figure out the more relevant answer
-	code, errors = get_code_in_answer_id(answers[0]['accepted_answer_id'])
-	return code, errors
+	code, url, errors = get_code_in_answer_id(answers[0]['accepted_answer_id'])
+	return code, url, errors
 
 class CodeGenerator:
 	def __init__(self, stopwords_file = None):
 		self.kw_extractor = KeywordsExtractor(stopwords_file)
 
-	def get_code_from_keywords(self, keywords, access_token = None, app_key = None):
-		code, errors = get_code(keywords, access_token, app_key)
-		return code, errors
+	def get_code_from_keywords(self, ip_str, keywords, access_token = None, app_key = None):
+		code, answer_link, errors = get_code(ip_str, keywords, access_token, app_key)
+		return code, answer_link, errors
 
 	def get_code_from_str(self, ip_str, kws_only = False, access_token = None, app_key = None):
 		kws = self.kw_extractor.get_keywords(ip_str)
 		if kws_only:
-			return '', kws, ''
-		code, errors = self.get_code_from_keywords(kws, access_token, app_key)
-		return code, kws, errors
+			return '', '', kws, ''
+		code, answer_link, errors = self.get_code_from_keywords(ip_str, kws, access_token, app_key)
+		return code, answer_link, kws, errors
 
--- src/keywords_extractor.py
+++ src/keywords_extractor.py
@@ -1,13 +1,21 @@
-import RAKE
+import RAKE, re
+
+p2_keywords = ['ArithmeticError', 'AssertionError', 'AttributeError', 'BaseException', 'BufferError', 'BytesWarning', 'DeprecationWarning', 'EOFError', 'Ellipsis', 'EnvironmentError', 'Exception', 'False', 'FloatingPointError', 'FutureWarning', 'GeneratorExit', 'IOError', 'ImportError', 'ImportWarning', 'IndentationError', 'IndexError', 'KeyError', 'KeyboardInterrupt', 'LookupError', 'MemoryError', 'NameError', 'None', 'NotImplemented', 'NotImplementedError', 'OSError', 'OverflowError', 'PendingDeprecationWarning', 'ReferenceError', 'RuntimeError', 'RuntimeWarning', 'StandardError', 'StopIteration', 'SyntaxError', 'SyntaxWarning', 'SystemError', 'SystemExit', 'TabError', 'True', 'TypeError', 'UnboundLocalError', 'UnicodeDecodeError', 'UnicodeEncodeError', 'UnicodeError', 'UnicodeTranslateError', 'UnicodeWarning', 'UserWarning', 'ValueError', 'Warning', 'ZeroDivisionError', '_', '__debug__', '__doc__', '__import__', '__name__', '__package__', 'abs', 'all', 'any', 'apply', 'basestring', 'bin', 'bool', 'buffer', 'bytearray', 'bytes', 'callable', 'chr', 'classmethod', 'cmp', 'coerce', 'compile', 'complex', 'copyright', 'credits', 'delattr', 'dict', 'dir', 'divmod', 'enumerate', 'eval', 'execfile', 'exit', 'file', 'filter', 'float', 'format', 'frozenset', 'getattr', 'globals', 'hasattr', 'hash', 'help', 'hex', 'id', 'input', 'int', 'intern', 'isinstance', 'issubclass', 'iter', 'len', 'license', 'list', 'locals', 'long', 'map', 'max', 'memoryview', 'min', 'next', 'object', 'oct', 'open', 'ord', 'pow', 'print', 'property', 'quit', 'range', 'raw_input', 'reduce', 'reload', 'repr', 'reversed', 'round', 'set', 'setattr', 'slice', 'sorted', 'staticmethod', 'str', 'sum', 'super', 'tuple', 'type', 'unichr', 'unicode', 'vars', 'xrange', 'zip','False', 'None', 'True', 'and', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+
 
 default_stopwords_file = "/home/ubuntu/code_assist_python/src/stopwords/SmartStoplist.txt"
 class KeywordsExtractor:
 	def __init__(self, stopwords_file = default_stopwords_file):
 		if not stopwords_file:
 			stopwords_file = default_stopwords_file
-		self.Rake = RAKE.Rake(stopwords_file)
+		self.Rake = RAKE.Rake(stopwords_file) #, p2_keywords)
 
-	def get_keywords(self, text_str):
+	def get_keywords(self, text):
+		reserved_kws = re.findall('"([^"]*)"', text)
+		text_str = text
+		for x in reserved_kws:
+			text_str = text_str.replace('"'+x+'"', "")
+
 		kws = self.Rake.run(text_str)
 		#TODO: value sorting?
-		return [x[0] for x in kws]
+		return [x[0] for x in kws] + reserved_kws
